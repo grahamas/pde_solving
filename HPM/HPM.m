60c4532874c15d52e4c87e88e1abeb4954c47f21
Graham Smith <grahamas@gmail.com>
2018-09-21 11:36:59 -0500

    [WIP] Needs rework
    so that H can be deriv wrt p at 0 at all recursion depths

--- HPM/HPM.m
+++ HPM/HPM.m
@@ -17,19 +17,49 @@ HPMTerms::usage =
 	expansion of the solution of A, using the homotopy perturbation method
 	with linear operator L and initial guess v0."
 
+HPMTerms[Lsym_, Asym_, guessfn_, n_] :=
+	ReleaseHold@ReplaceAll[Hold[SolveDeformation[ 
+		LinearSolver[Lsym, p], 
+		SolutionHomotopy[Lsym,Asym,v0][v, p],
+		v, 
+		v0,
+		p,
+		n]],{v0 -> guessfn @@ SpatioTemporalParams[NSpaceDims[Asym]], 
+			v -> DeformingFunction[NSpaceDims[Asym], p]}]
+
+Operator[Burgers1] = (D[#,t] + # D[#,x1] + D[#,{x1,2}])&
+NSpaceDims[Burgers1] = 1
+InitialGuess[Burgers1][x_,t_] = (\[Alpha] + \[Beta] + (\[Beta] - \[Alpha]) e^\[Gamma])/(
+ 1 + e^\[Gamma]) /. {\[Gamma] -> \[Alpha]/\[Epsilon] (x - \[Lambda])}
+
+Operator[TimeDerivative] = (Derivative[#,t])&
+LinearSolver[TimeDerivative, p_][lastv_, lastH_, pastsolns_] :=
+	MySimplify[Integrate[Solve[D[lastH,p], D[D[lastv, p], t]], t]]
+
 Begin["`Private`"]
 (* Implementation of the package *)
 
-SolutionHomotopy[L_, A_, v0_][v_, q_] := (1-q) L[v - v0] + q A[v]
+MySimplify[expr_] := FullSimplify[expr /. {Log[e] -> 1}]
+
+DeformingFunction[n_, p_] := Phi @@ DeformingFunctionParams[n, p]
+
+SpatioTemporalParams[1] := {x, t}
+SpatioTemporalParams[n_] := {Sequence[Array[x,n]], t}
+DeformingFunctionParams[n_, p_] := {Sequence @@ SpatioTemporalParams[n], p}
+
+SolutionHomotopy[Lsym_, Asym_, v0_][v_, q_] := (1-q) Operator[Lsym][v - v0] + q Operator[Asym][v]
+
+(* TODO: Untested for multi-variable *)
+SolveDeformation[solver_, H_, v_, v0_List, p_, 0] :=
+	Sequence @@ {v, MapThread[((# /. p -> 0) -> #2)&, {v, v0}], H}
+	
+(* TODO: Untested for one-variable *)
+SolveDeformation[solver_, H_, v_, v0_, p_, 0] :=
+	Sequence @@ {v, (v /. p -> 0) -> v0, H}
 
 (* TODO: Not currently maintaining old derivatives, which would be more efficient *)
-HPMFromHomotopy[H_, solver_, v_, n_] :=
-	solver[v, HPMFromHomotopy[H, solver, v, n-1]]
-
-HPMTerms[Lsym_, Asym_, v0_, n_] :=
-	HPMFromHomotopy[SolutionHomotopy[Lsym,Asym,v0], LinearSolver[Lsym, NVars[Asym]], DeformingFunction[NVars[Asym]], n]
-
-
+SolveDeformation[solver_, H_, v_, v0_, p_, n_] :=
+	solver[SolveDeformation[solver, H, v, v0, p, n-1]]
 
 End[]
 
---
+++ HPM/Operators.m
@@ -0,0 +1,22 @@
+(* Wolfram Language Package *)
+
+BeginPackage["Operators`"]
+(* Exported symbols added here with SymbolName::usage *)  
+
+Operator::usage = 
+	"Operator[symbol] returns differential operator."
+	
+NSpaceDims::usage = 
+	"NSpaceDims[symbol] returns the number of space dimensions."
+
+
+	
+	
+Begin["`Private`"] (* Begin Private Context *) 
+
+
+
+
+End[] (* End Private Context *)
+
+EndPackage[]

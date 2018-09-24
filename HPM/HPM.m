d70e361b5ed7bf822ff8df8709652aa6d1fd9620
Graham Smith <grahamas@gmail.com>
2018-09-24 15:28:01 -0500

    Works for single-valued equation.
    Don't try large n.

--- HPM/HPM.m
+++ HPM/HPM.m
@@ -20,55 +20,62 @@ HPMTerms::usage =
 HPMTerms[Lsym_, Asym_, guessfn_, n_] :=
 	ReleaseHold@ReplaceAll[Hold[SolveDeformation[ 
 		LinearSolver[Lsym, Asym, v, v0], 
+		Asym,
 		v, 
 		v0,
-		p,
-		n]],{v0 -> guessfn @@ SpatioTemporalParams[NSpaceDims[Asym]], 
+		n]],{v0 -> guessfn[SpatioTemporalParams[Asym]], 
 			v -> Phi}]
 
 (* Burgers1-specific definitions *)
-Operator[Burgers1] = (D[#,t] + # D[#,x1] + D[#,{x1,2}])&
+Operator[Burgers1] = (D[#,t] + # D[#,x] + D[#,{x,2}])&
 NSpace[Burgers1] = 1
 InitialGuess[Burgers1][x_,t_] = (\[Alpha] + \[Beta] + (\[Beta] - \[Alpha]) e^\[Gamma])/(
  1 + e^\[Gamma]) /. {\[Gamma] -> \[Alpha]/\[Epsilon] (x - \[Lambda])}
-RelevantDerivatives[Burgers1, pastsolns_List] := Catenate[RelevantDerivatives /@ pastsolns]
+RelevantDerivatives[Burgers1, pastsolns_List] := Catenate[(RelevantDerivatives[Burgers1,#])& /@ pastsolns]
 RelevantDerivatives[Burgers1, pastsoln_] := {pastsoln, D[pastsoln, x], D[pastsoln, {x,2}]}
 
 (* TimeDerivative linear operator definitions *) 
-Operator[TimeDerivative] = (Derivative[#,t])&
+Operator[TimeDerivative] = (D[#,t])&  (* WARNING: Is this right??? Might need to be Derivative and specify by position *)
 LinearSolver[TimeDerivative, Asym_, v_, v0_][pastsolns_, n_] :=
-	MySimplify[OneVariableSolve[
-		UsePastSolns[Asym, 
+	OneVariableSolveZeroBoundary[
+		UsePastSolns[
+			Asym, 
 			Deformation[SolutionHomotopy[TimeDerivative,Asym,v0],v,n], 
 			pastsolns], 
-		DerivP[Asym,v,n], t]]
+		DerivP[Asym,v,n], t][[1]]
 
-Begin["`Private`"]
 (* Implementation of the package *)
 
+Begin["`Private`"]
 (* General definitions *)
 UsePastSolns[Asym_, deformation_, pastsolns_] := deformation /. RelevantDerivatives[Asym, pastsolns]
 Deformation[H_,v_,n_] := Derivative[0,n][H][v,0]
-DerivP[Asym_, v_, i_] := ((Derivative @@ {Sequence @@ ConstantArray[0, NSpace[Asym]+1], i}) @ v) @@ {SpatioTemporalParams[NSpace[Asym]],0} 
-OneVariableSolve[eqn_, target_, var_] := Integrate[Solve[eqn, D[target,var]], var]
+DerivP[Asym_, v_, i_] := ((Derivative @@ {Sequence @@ ConstantArray[0, NSpace[Asym]+1], i}) @ v)[P0Params[NSpace[Asym]]]
+OneVariableSolveZeroBoundary[eqn_, target_, var_] := 
+	MySimplify[Solve[Integrate[eqn, var] == 0, target]]
 MySimplify[expr_] := FullSimplify[expr /. {Log[e] -> 1}]
 
 SpatioTemporalParams[1] := Sequence[x, t]
-SpatioTemporalParams[n_] := Sequence[Sequence @@ Array[x,n], t]
+SpatioTemporalParams[n_Integer] := Sequence[Sequence @@ Array[x,n], t]
+SpatioTemporalParams[sym_Symbol] := SpatioTemporalParams[NSpace[sym]]
+DeformationParams[norsym_, p_] := Sequence @@ {SpatioTemporalParams[norsym], p}
+P0Params[norsym_] := Sequence @@ {SpatioTemporalParams[norsym], 0}
 
-SolutionHomotopy[Lsym_, Asym_, v0_][v_, q_] := (1-q) Operator[Lsym][v - v0] + q Operator[Asym][v]
+
+SolutionHomotopy[Lsym_, Asym_, v0_][v_, q_] := (1-q) Operator[Lsym][v[DeformationParams[Asym,q]] - v0] + q Operator[Asym][v[DeformationParams[Asym,q]]]
 
 (* TODO: Untested for multi-variable *)
-SolveDeformation[solver_, H_, v_, v0_List, p_, 0] :=
-	Sequence @@ {v[SpatioTemporalParams[Asym],0], MapThread[((# /. p -> 0) -> #2)&, {v, v0}], H}
+SolveDeformation[solver_, Asym_, v_, v0_List, 0] :=
+	MapThread[(#[P0Params[NSpace[Asym]]] -> #2)&, {v, v0}]
 	
 (* TODO: Untested for one-variable *)
-SolveDeformation[solver_, H_, v_, v0_, p_, 0] :=
-	Sequence @@ {v, (v /. p -> 0) -> v0, H}
+SolveDeformation[solver_, Asym_, v_, v0_, 0] :=
+	 {v[P0Params[NSpace[Asym]]] -> v0}
 
 (* TODO: Not currently maintaining old derivatives, which would be more efficient *)
-SolveDeformation[solver_, H_, v_, v0_, p_, n_] :=
-	solver[SolveDeformation[solver, H, v, v0, p, n-1]]
+SolveDeformation[solver_, Asym_, v_, v0_, n_] := 
+	ReleaseHold@ReplaceAll[Hold[Catenate[{pastsoln, solver[pastsoln, n]}]], 
+		pastsoln -> SolveDeformation[solver, Asym, v, v0, n-1]]
 
 End[]
 
